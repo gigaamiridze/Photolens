fa04499d8e6956ae799a32f216e28b83cf7cd5e2
Flame <flameyungoxx@gmail.com>
2022-04-20 17:57:16 +0400

    added product remove function from the cart

--- static/js/shopping-cart.js
+++ static/js/shopping-cart.js
@@ -22,3 +22,15 @@ closeCart.onclick = () => {
 function closeBackground() {
     $('body').removeClass('open');
 }
+
+// Remove product from cart
+const removeBtn = document.getElementsByClassName("remove-btn");
+for (var i = 0; i < removeBtn.length; i++) {
+  button = removeBtn[i];
+  button.addEventListener('click', removeItem);
+}
+
+function removeItem(event) {
+  btnClicked = event.target;
+  btnClicked.parentElement.parentElement.parentElement.remove();
+}
